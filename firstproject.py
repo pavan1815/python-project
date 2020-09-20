78bdc077758906cabe59298a615e1858e61909b6
pavan1815 <71280174+pavan1815@users.noreply.github.com>
2020-09-20 19:15:39 +0530

    1st project
    1st project

---
+++ firstproject.py
@@ -0,0 +1,249 @@
+#PROJECT TO KNOW HOROSCORE OF THE DAY
+nextEntry = True
+while nextEntry == True:
+          
+
+  print('''
+  
+  1.Aries          |          2.Taurus          |          3.Gemini
+                   |                            |
+  4.Cancer         |          5.Leo             |          6.Virgo
+                   |                            | 
+  7.Libra          |          8.Scorpio         |          9.Sagittarius
+                   |                            | 
+  10.Capricorn     |         11.Aquarius        |         12.Pisces
+  
+  ''')
+  print("Please pick your zodiac sign by entering the respected number and hit the enter button to get your horoscope:\n")
+  numb = int(input())
+  if numb == 1:
+    print('''
+     *Aries (March 21-April 20): There is a good chance of getting a better bargain on something that is going beyond your budget. Work front will seem inviting to some as a new colleague of the other gender joins in. Timely advice of an elder is likely to smooth out a turbulent relationship. Property matters causing tensions are likely to be resolved amicably. A change brings some freshness along with it and helps you unwind. Joining a gym or starting a fitness routine is possible and will benefit immensely. If you get a chance to see a new place, don’t miss the opportunity.
+    
+     Love Focus: Love is in the air, but you will need to bring lover to your wavelength for enjoying it to the hilt!
+    
+     Lucky Colour: Pink
+    
+     Lucky Alphabet: M
+    
+     Friendly Numbers: 20, 22
+    
+     Friendly Zodiac Today: Pisces & Cancer
+    
+     Be careful of: Libra
+    
+     ''')
+  
+  
+  elif numb==2:
+     print('''
+     
+      *Taurus (April 21-May 20): You may be cool about a domestic situation that warrants immediate action. Entrepreneurs will need to be more deliberate in putting a plan into action. Money may start flowing in from a different source and make you financially secure. Improvement in the condition of those unwell is foreseen. Delays and disruptions in a journey need to be taken in your stride. Promoting your self-interest may be on your mind and you will take steps to achieve the same. Some of you are likely to become the proud owners of a landed property.
+  
+      Love Focus: Finding someone with common interests and tastes may ring in a budding romance and end your lonely days!
+  
+  
+      Lucky Colour: Lavender
+  
+      Lucky Alphabet: V
+  
+      Friendly Numbers: 4, 11
+  
+      Friendly Zodiac Today: Sagittarius & Libra
+  
+      Be careful of: Aquarius
+      
+     ''')
+   
+  
+  elif numb==3:  
+     print('''
+  *Gemini (May 21-Jun 21): Spouse will approve of your idea regarding an issue without much convincing. A loan applied for may take its own time in materialising, but will be released in good time. Your firm resolution to remain totally fit may find you burning the track! Don’t remind senior about a job that remains incomplete as you may have to burn the midnight oil! You will take a step nearer to resolving a property dispute. There is no point in getting oversensitive about issues that have no long-term effects. Travelling to a distant place with lover becomes a blessed journey of togetherness.
+  
+  
+  Love Focus: Taking steps to rejuvenate your love life is likely to get the excitement back into your life.
+  
+  Lucky Colour: Indigo
+  
+  Lucky Alphabet: H
+  
+  Friendly Numbers: 6, 9
+  
+  Friendly Zodiac Today: Leo & Scorpio
+  
+  Be careful of: Pisces
+     ''')
+  
+  elif numb==4:
+     print('''
+      *Cancer (Jun 22-July 22): Financial worries become a thing of the past, as money comes to you from an unexpected source. Health of a family member may cause a bit of a concern, but will be nothing serious. Remain regular in your workouts, if you are serious of coming back in shape. Those heading for a vacation are poised to enjoy every moment of it! Possession of a house or apartment is likely to be offered soon. Someone may influence you to change your outlook in life and it will be for your own good. Celebrations regarding a promotion or a raise are just round the corner for some.
+  
+  
+  Love Focus: Romantic mood prevails and chances of a candle light dinner thrown in, cannot be ruled out.
+  
+  Lucky Colour: Metallic Blue
+  
+  Lucky Alphabet: B
+  
+  Friendly Numbers: 2, 4,
+  
+  Friendly Zodiac Today: Capricorn & Scorpio
+  
+  Be careful of: Sagittarius
+     ''')
+  
+  elif numb==5:
+     print('''
+  *Leo (July 23-August 23): Sticking to routine will find you fit and energetic on the health front. Improvements in ancestral house are likely to be carried out. Your networking capabilities will smoothen the process of getting something officially done. You are likely to find your financial circumstances improving, as money begins to flow in. Focus of a family youngster may waver, if you don’t give the right direction to him or her. You will be beside yourself with happiness as a most pleasant surprise comes your way. You may opt for going Dutch with friends, if you don’t want any extra financial burden on your head.
+  
+  
+  Love Focus: You will be impressed by whatever little you know about someone, who has recently come into your life.
+  
+  Lucky Colour: Green
+  
+  Lucky Alphabet: R
+  
+  Friendly Numbers: 6, 9
+  
+  Friendly Zodiac Today: Scorpio & Taurus
+  
+  Be careful of: Aries
+     ''')
+  
+  
+  elif numb==6:
+     print('''
+  *Virgo (August 24-September 23): Daydreaming on the road is an invitation for trouble, so remain alert at all times. Wealth or property may come to some through inheritance. Your man management skills may need to be honed, if you want to remain effective at work. Your serious attempts to come back in shape will start showing positive results soon. A stress-free environment will seem a godsend and will help you prosper. Financially you will find yourself in a comfortable situation, but it could have been better. It may take some doing to bring a family elder to your way of thinking, so keep up the efforts.
+  
+  
+  Love Focus: Someone you desire will take the initiative of spending time with you on the romantic front.
+  
+  Lucky Colour: Off White
+  
+  Lucky Alphabet: G
+  
+  Friendly Numbers: 3, 7
+  
+  Friendly Zodiac Today: Sagittarius & Leo
+  
+  Be careful of: Cancer
+     ''')
+  
+  
+  elif numb==7:
+     print('''
+  
+  *Libra (September 24-October 23): Those travelling on a long journey must set out well prepared for contingencies. Consider your financial worries over, as you are about to hit it big on the financial front. Family life cruises along smoothly, as you follow your set routine undisturbed. You are likely to put in extra efforts on the fitness front for an ideal physique. Excellent returns can be expected from sale of property. Retaining someone who is important to you is a necessity that you just cannot escape. New ventures can be thought of by some, but implementing them can pose difficulties.
+  
+  Love Focus: You may get the courage to express your love for someone you have a soft corner for.
+  
+  Lucky Colour: Turquoise
+  
+  Lucky Alphabet: D
+  
+  Friendly Numbers: 8, 10
+  
+  Friendly Zodiac Today: Aquarius & Taurus
+  
+  Be careful of: Pisces
+     ''')
+  
+  
+  elif numb==8:
+     print('''
+  *Scorpio (October 24-November 22): Money comes in a steady stream and promises to make you financially comfortable. Thorough knowledge about your trade is likely to place your head and shoulders above others. Time spent with family is likely to ease your stress. Not succumbing to temptations will keep your body and mind in a sound condition. An inheritance may come your way in the form of property or cash. Those fighting boredom and isolation can breathe easy today, as someone steps in to brighten up their day. An overseas business cum leisure trip is in the offing and will prove extremely profitable.
+  
+  Love Focus: Mutual attraction is likely to draw you closer to someone on the romantic front, so expect an exciting phase to begin!
+  
+  Lucky Colour: Metallic Blue
+  
+  Lucky Alphabet: H
+  
+  Friendly Numbers: 1, 4,
+  
+  Friendly Zodiac Today: Virgo & Taurus
+  
+  Be careful of: Aries
+     ''')
+  
+  
+  elif numb==9:
+     print('''
+  *Sagittarius (November 23-December 21): Earning from multiple sources is indicated, so expect your bank balance to remain in a healthy state. You will be able to give an excellent account of yourself at work and be praised for the same. Parents or a family elder may try to curb your liberty, so be at your tactful best! Someone’s advice on the fitness front will prove most beneficial in retaining perfect health. Property dealers are likely to make a killing in the realty market. You will manage to organise a lavish feast for which you had been saving. If you are undertaking a long journey, it will be in your interest to go well prepared.
+  
+  Love Focus: The best is yet to come, but you will find the present also as scintillating on the romantic front!
+  
+  Lucky Colour: Chocolate
+  
+  Lucky Alphabet: K
+  
+  Friendly Numbers: 4, 8
+  
+  Friendly Zodiac Today: Taurus & Cancer
+  
+  Be careful of: Gemini
+     ''')
+  
+  
+  
+  elif numb==10:
+     print('''
+  *Capricorn (December 22-January 21): A healthy bank balance is likely to instill confidence in you on the financial front. Differences with spouse cannot be ruled out, but avoid confrontation. Only a professionally sound strategy will succeed in the kind of competitive environment that you are in. Exercising self-control will be the key to keeping a lifestyle disease in check for those suffering from it. If you have the money, buy the property you had been eyeing for long. Possibility of spending time with some old friends seems quite bright. Someone may invite you to visit to a historical monument or some other place of interest today. A real estate transaction is likely to prove profitable.
+  
+  Love Focus: Someone may be expecting a positive response from you on the romantic front, but do what your heart dictates.
+  
+  Lucky Colour: Dark Turquoise
+  
+  Lucky Alphabet: S
+  
+  Friendly Numbers: 2, 4, 6
+  
+  Friendly Zodiac Today: Leo & Aries
+  
+  Be careful of: Virgo
+     ''')
+  
+  
+  elif numb==11:
+     print('''
+  *Aquarius (January 22-February 19): You may not be too happy with your current financial situation and do something about it. A domestic event will go smoothly under your watchful eye. Things move smoothly at work as you proceed steadily towards completion of a project. Home remedy is likely to work wonders for a minor ailment and restore your health. You will be at your creative best and manage to achieve complete peace of mind in the process. A long journey will prove enjoyable in more ways than one, as you find love! Taking possession of a new house may become a reality for some.
+  
+  Love Focus: Your romantic endeavours are likely to elicit a positive response from the one you like.
+  
+  Lucky Colour: Golden Brown
+  
+  Lucky Alphabet: L
+  
+  Friendly Numbers: 9, 12
+  
+  Friendly Zodiac Today: Gemini & Leo
+  
+  Be careful of: Cancer
+     ''')
+  
+  
+  elif numb==12:
+     print('''
+  *Pisces (February 20-March 20): You may get a chance to enjoy a family get-together soon. You may decide to take up an exercise regime and benefit on the health front.. Adventurous types can expect a time of their lives today in a thrilling outdoor activity. You may decide to take up residence near your place of work. An exclusive outing with the one who matters is on the cards, so expect a fun-filled evening. Professionally, you are likely to excel and rub shoulders with the best. Some more avenues of earning are likely to open up soon and make you financially secure
+  
+  Love Focus: Someone you have a crush on is likely to send positive signals, so don’t let this opportunity slips through your fingers!
+  
+  Lucky Colour: Peach
+  
+  Lucky Alphabet: J
+  
+  Friendly Numbers: 3, 9
+  
+  Friendly Zodiac Today: Aries & Libra
+  
+  Be careful of: Taurus
+     ''')   
+  
+  
+  else: 
+     print("Please enter the correct number")
+  
+  nextEntry=True if input("\nshall we do it again if yes enter Y otherwise enter N?\n") =="Y" else False
+   
+   
+    
